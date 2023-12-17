3a10a24f8039352046a4219264a3aafb94a5a7b5
nagendhra224 <pdattanagendhra8@gmail.com>
2023-12-17 08:33:34 +0000

    app

---
+++ src/components/TimelineView/styledComponents.js
@@ -0,0 +1,46 @@
+/* Write your CSS here */
+import styled from 'styled-components/macro'
+
+export const TimelineContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  min-height: 100vh;
+`
+export const ResponsiveContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 1110vh;
+  height: 100vh;
+`
+export const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 30px;
+`
+
+export const Heading = styled.h1`
+  text-align: center;
+  color: #171f46;
+  font-family: 'roboto';
+  font-weight: 500;
+  line-height: 2.7;
+  margin: 0px;
+  @media screen and (min-width: 768px) {
+    font-size: 20px;
+  }
+`
+
+export const CCBPHeading = styled.span`
+  color: #2b237c;
+  font-family: 'roboto';
+  font-weight: 500;
+  font-size: 24px;
+  margin: 0px;
+  @media screen and (min-width: 768px) {
+    font-size: 30px;
+  }
+`
